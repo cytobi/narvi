2694fbb3738b9228cda891c297a24dea11428a50
cytobi <tobi81548@gmail.com>
2023-07-21 14:42:13 +0200

    added tags to addtask.sh

--- src/addtask.sh
+++ src/addtask.sh
@@ -8,6 +8,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/../etc/config.sh
 source $SCRIPT_DIR/../etc/secrets.sh
 
+# check whether task name was provided
+if [ -z "$1" ]; then
+    echo "Please enter a task name"
+    exit 1
+fi
+
+# check whether tag was provided
+TAGPROPERTY=''
+if [ ! -z "$2" ]; then
+    TAGPROPERTY=', "Tags": { "multi_select": [ { "name": "'"$2"'" } ] }'
+fi
+
 # create a new task
 curl -s 'https://api.notion.com/v1/pages' \
   -H 'Authorization: Bearer '"$NOTION_API_KEY"'' \
@@ -24,8 +36,13 @@ curl -s 'https://api.notion.com/v1/pages' \
             }
           }
         ]
-      }
+      }'"$TAGPROPERTY"'
     }
   }' > /dev/null
 
+# different output depending on whether tag was provided
+if [ ! -z "$2" ]; then
+    echo 'added task "'$1'" to tag "'$2'"'
+    exit 0
+fi
 echo 'added task "'$1'"'
