716912bab533b5969fa82efd3bac6d1b17fe9ab1
cytobi <tobi81548@gmail.com>
2023-07-21 11:43:22 +0200

    setup script & jq
    - add setup script
    - update readme accordingly
    - add jq to getdb.sh

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ a notion cli written in bash, tailored to my needs specifically
 
 ## Setup
 - create a notion integration
-- copy the api key
-- create etc/secrets.sh
-- add `#!/bin/bash` to secrets.sh
-- add `NOTION_API_KEY="<your_api_key>"` to secrets.sh
+- take note of the api key
 - [get the database id](https://developers.notion.com/reference/retrieve-a-database) of the database you want to use
-- add `NOTION_DB_ID="<your_database_id>"` to secrets.sh
+- navigate to the narvi directory (where this README is)
+- run `bash setup.sh`
+- enter the api key when prompted
+- enter the database id when prompted
+- allow narvi to access this database by opening the meatballs menu (the three dots) in the upper right corner and clicking "Add connections" and then "narvi"
+- run `bash src/getdb.sh` to get the database and test if everything works
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# install dependencies and setup the environment
+# this script is meant to be run from the root of the project
+
+# check whether in root of project
+if [ ! -f "setup.sh" ]; then
+    echo "Please run this script from the root of the project"
+    exit 1
+fi
+
+# install dependencies
+# check whether jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found, installing..."
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install jq -y
+fi
+
+# create non-existent environment files
+touch etc/secrets.sh
+echo "#!/bin/bash" > etc/secrets.sh
+echo "Please enter your Notion API key:"
+read NOTION_API_KEY
+echo "NOTION_API_KEY=\"$NOTION_API_KEY\"" >> etc/secrets.sh
+echo "Please enter your Notion database ID:"
+read NOTION_DB_ID
+echo "NOTION_DB_ID=\"$NOTION_DB_ID\"" >> etc/secrets.sh
+
+echo "Setup complete!"
